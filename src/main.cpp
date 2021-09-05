6915f1c53f1541d3b91bce63411f5e2ce278856d
Gabriel <gabriel_dxgpx3@hotmail.com>
2021-09-05 13:52:34 -0300

    Emergency mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 #include <Wire.h>
 #include <IRremote.h>
 #include <RTClib.h>
+#include <EEPROM.h>
 
-#define HORA_LIGAR 15
-#define MINUTO_LIGAR 40
+#define DELTA_EMERGENCY_TIME 30
+
+#define HORA_LIGAR 18
+#define MINUTO_LIGAR 00
 #define SEGUNDO_LIGAR 00
 
-#define HORA_DESLIGAR 15
-#define MINUTO_DESLIGAR 52
+#define HORA_DESLIGAR 23
+#define MINUTO_DESLIGAR 20
 #define SEGUNDO_DESLIGAR 00
 
 #define CODE_ON 0x20DF8E71
@@ -16,12 +19,22 @@
 
 #define ATUADOR_PIN 8
 #define IR_PIN 11
+//#define DEBUG 1
+//#define AJUST_TIME 1
+
+struct EEPromTime{
+    int hour;
+    int minute;
+    int second;
+};
 
 struct timeTrigger{
     int hora;
     int minuto;
     int segundo;
 };
+
+
 
 struct Atuador{
     const int pin;
@@ -31,12 +44,18 @@ struct Atuador{
     }
 
     void on() const {
-        //Serial.println("ligou rele");
+        #ifdef DEBUG
+            Serial.println("ligou rele");
+        #endif
+
         digitalWrite(pin, HIGH);
     }
 
     void off() const {
-        //Serial.println("Desligou rele");
+        #ifdef DEBUG
+            Serial.println("Desligou rele");
+        #endif
+    
         digitalWrite(pin, LOW);
     }
 
@@ -69,32 +88,105 @@ RTC_DS3231 rtc;
 IRrecv irReceiver(IR_PIN);
 decode_results iRresults;
 DateTime now;
+byte emergencyMode = 0;
 
 bool isInTriggerTime(){
     now = RTC_DS3231::now();
     return now.hour() >= ligar.hora && (now.hour() <= desligar.hora && now.minute() < desligar.minuto);
 }
 
+EEPromTime getEEPromTime(){
+
+    EEPromTime time{
+        EEPROM.read(0),
+        EEPROM.read(1),
+        EEPROM.read(2),
+    };
+
+    return time;
+}
+
+bool isEmergencyMode(){
+    now = RTC_DS3231::now();
+    EEPromTime time = getEEPromTime();
+
+    #ifdef DEBUG
+        Serial.println("EEPROM HOUR: " + String(time.hour));
+        Serial.println("EEPROM MINUTE: " + String(time.minute));
+        Serial.println("EEPROM SECOND: " + String(time.second));
+    #endif  
+
+    return now.hour() == time.hour && now.minute() == time.minute && (now.second() - time.second) <= DELTA_EMERGENCY_TIME;
+}
+
+void saveLastPowerOn(){
+    now = RTC_DS3231::now();
+
+    EEPROM.write(0, now.hour());
+    EEPROM.write(1, now.minute());
+    EEPROM.write(2, now.second());
+}
+
+void emergencyModeActions(){
+
+    #ifdef DEBUG
+        Serial.println("Rele ligado!");
+    #endif
+
+    rele.on();
+    delay(200);
+    rele.off();
+    delay(200);
+    rele.on();
+    saveLastPowerOn();
+}
+
 void setup(){
-    //Serial.begin(9600);
-    irReceiver.enableIRIn();
+
+    #ifdef DEBUG
+        Serial.begin(9600);
+    #endif
 
     if(!rtc.begin()){
         while(1);
     }
 
-    if(isInTriggerTime() && rele.isOff()){
-        rele.on();
-    }else if(!isInTriggerTime() && rele.isOn()){
-        rele.off();
-    }
+    #ifdef AJUST_TIME
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    #endif
 
-    delay(100);
+    if(isEmergencyMode()){
+        emergencyMode = 1;
+        emergencyModeActions();
+
+        #ifdef DEBUG
+            Serial.println("EMERGENCY MODE!!");
+        #endif
+
+    }else{
+
+        #ifdef DEBUG
+            Serial.println("NORMAL MODE!!");
+        #endif
+
+        saveLastPowerOn();
+
+        irReceiver.enableIRIn();
+
+        if(isInTriggerTime() && rele.isOff()){
+            rele.on();
+        }else if(!isInTriggerTime() && rele.isOn()){
+            rele.off();
+        }
+
+        delay(100);
+
+    }
 }
 
 bool isTimeOf(timeTrigger* trigger){
     now = RTC_DS3231::now();
-    return trigger->segundo == now.second() && trigger->minuto == now.minute() && trigger->hora == now.hour();;
+    return trigger->segundo == now.second() && trigger->minuto == now.minute() && trigger->hora == now.hour();
 }
 
 void readIr(){
@@ -114,9 +206,14 @@ void readIr(){
 }
 
 void loop(){
+
+    while(emergencyMode){}
+
     now = RTC_DS3231::now();
 
-    //Serial.println(String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()));
+    #ifdef DEBUG
+        Serial.println(String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()));
+    #endif
 
     readIr();
 
